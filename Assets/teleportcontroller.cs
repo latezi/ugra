4b808b86a32b1dcd621129529945a89170c80670
fgdhfgdh <xxanov2017@mail.ru>
2021-06-24 11:17:54 +0300

    normalnue teleporty

---
+++ Assets/teleportcontroller.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+public class teleportcontroller : MonoBehaviour
+{
+    public GameObject basecontroller;
+    public GameObject teleportaction;
+
+    public InputActionReference telepotrActionReference;
+    [Space]
+    public UnityEvent onteleportactivate;
+    public UnityEvent onteleportcancel;    
+        
+    void Start()
+    {
+        telepotrActionReference.action.performed += teleportmodeactivated;
+        telepotrActionReference.action.canceled += teleportmodecancel;
+    }
+
+    private void teleportmodecancel(InputAction.CallbackContext obj)
+    {
+        Invoke("DeactivateTeleporter", .1f);
+    }
+
+    void DeactivateTeleporter()
+    {
+        onteleportcancel.Invoke();
+    }
+    private void teleportmodeactivated(InputAction.CallbackContext obj)
+    {
+        onteleportactivate.Invoke();
+    }
+}
